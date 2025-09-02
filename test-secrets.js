9376ce75b3126ef4e1ea0b189920a312bec8dfb0
Nahidul Islam <nahidkishore99@gmail.com>
2025-09-02 16:26:24 +0600

    Update test-secrets.js

--- test-secrets.js
+++ test-secrets.js
@@ -1,9 +1,9 @@
 
-// Fake API key for testing Gitleaks
-//const stripeApiKey = "sk_test_51HxxxxxFYYxxKxxxxxx";
+ //Fake API key for testing Gitleaks
+const stripeApiKey = "sk_test_51HxxxxxFYYxxKxxxxxx";
 
-// Demo password
-//const dbPassword = "mySuperSecret123";
+//Demo password
+const dbPassword = "mySuperSecret123";
 
 // AWS secret for test
-//const awsSecretAccessKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+const awsSecretAccessKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
